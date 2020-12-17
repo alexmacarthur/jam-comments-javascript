b3b50872985650a3f3e5a4db74fb691da5302f05
Alex MacArthur <alex@macarthur.me>
2020-12-16 21:32:24 -0600

    build again.

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,14 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module",
   },
+  settings: {
+    react: {
+      version: "17.0",
+    },
+  },
   plugins: ["react", "@typescript-eslint"],
-  rules: {},
+  rules: {
+    "react/display-name": 0,
+    "react/prop-types": 0,
+  },
 };
