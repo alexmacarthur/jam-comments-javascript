be88721df2b1207571e3b5ffe0f95fa6236c2938
Alex MacArthur <alex@macarthur.me>
2021-01-22 21:38:53 -0600

    Add missing props to Next component.

--- packages/next/src/index.tsx
+++ packages/next/src/index.tsx
@@ -2,8 +2,14 @@ import * as React from "react";
 import JamCommentsReact from "@jam-comments/react/dist/index.es.js";
 const { CommentFetcher } = require("@jam-comments/utilities/server");
 
-export const JamComments = ({ comments }) => {
-  return <JamCommentsReact initialComments={comments} />;
+export const JamComments = ({ comments, apiKey, domain }) => {
+  return (
+    <JamCommentsReact
+      initialComments={comments}
+      apiKey={apiKey}
+      domain={domain}
+    />
+  );
 };
 
 export const fetchByPath = async ({ apiKey, domain, path }) => {
