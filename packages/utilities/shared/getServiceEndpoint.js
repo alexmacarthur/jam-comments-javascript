ff6591a706800aaa994c067429501e6049bae5a6
Alex MacArthur <alex@macarthur.me>
2021-03-07 22:21:27 -0600

    Fix markup and styling issues in Eleventy plugin.

--- packages/utilities/shared/getServiceEndpoint.js
+++ packages/utilities/shared/getServiceEndpoint.js
@@ -1,11 +1,10 @@
 const SERVICE_ENDPOINT = "https://service.jamcomments.com";
 
 const getServiceEndpoint = (endpoint) => {
-  const ENVIRONMENT_SERVICE_ENDPOINT = process && process.env && process.env.JAM_COMMENTS_SERVICE_ENDPOINT;
+  const ENVIRONMENT_SERVICE_ENDPOINT =
+    process && process.env && process.env.JAM_COMMENTS_SERVICE_ENDPOINT;
 
-  return (
-    endpoint || ENVIRONMENT_SERVICE_ENDPOINT || SERVICE_ENDPOINT
-  );
+  return endpoint || ENVIRONMENT_SERVICE_ENDPOINT || SERVICE_ENDPOINT;
 };
 
 module.exports = getServiceEndpoint;
