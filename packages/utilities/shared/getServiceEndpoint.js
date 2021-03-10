af3745880e518bce5442473fa202c05efaf2df79
Alex MacArthur <alex@macarthur.me>
2021-03-09 21:59:00 -0600

    Fix utilities issue, add tests.

--- packages/utilities/shared/getServiceEndpoint.js
+++ packages/utilities/shared/getServiceEndpoint.js
@@ -1,10 +1,13 @@
-const SERVICE_ENDPOINT = "https://service.jamcomments.com";
+const DEFAULT_SERVICE_ENDPOINT = "https://service.jamcomments.com";
 
 const getServiceEndpoint = (endpoint) => {
+  const processIsDefined = typeof process !== "undefined";
   const ENVIRONMENT_SERVICE_ENDPOINT =
-    process && process.env && process.env.JAM_COMMENTS_SERVICE_ENDPOINT;
+    processIsDefined &&
+    process.env &&
+    process.env.JAM_COMMENTS_SERVICE_ENDPOINT;
 
-  return endpoint || ENVIRONMENT_SERVICE_ENDPOINT || SERVICE_ENDPOINT;
+  return endpoint || ENVIRONMENT_SERVICE_ENDPOINT || DEFAULT_SERVICE_ENDPOINT;
 };
 
 module.exports = getServiceEndpoint;
---
+++ packages/utilities/shared/getServiceEndpoint.test.js
@@ -0,0 +1,35 @@
+import getServiceEndpoint from "./getServiceEndpoint";
+
+const OLD_ENV = process.env;
+
+beforeEach(() => {
+  jest.resetModules();
+});
+
+afterAll(() => {
+  process.env = OLD_ENV;
+});
+
+test("it should return production service endpoint when no override is set.", () => {
+  process.env = {};
+
+  const result = getServiceEndpoint();
+
+  expect(result).toEqual("https://service.jamcomments.com");
+});
+
+test("it should use override env variable when it is set.", () => {
+  process.env = {
+    JAM_COMMENTS_SERVICE_ENDPOINT: "http://localhost:4000",
+  };
+
+  const result = getServiceEndpoint();
+
+  expect(result).toEqual("http://localhost:4000");
+});
+
+test("it should use parameter when provided", () => {
+  const result = getServiceEndpoint("http://test.com");
+
+  expect(result).toEqual("http://test.com");
+});
