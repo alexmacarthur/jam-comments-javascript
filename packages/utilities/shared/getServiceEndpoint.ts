437cb2a47143853cda86780efced8e147c7d2d27
Alex MacArthur <alex@macarthur.me>
2021-11-05 23:11:28 -0500

    Fix ENV issues causing wrong endpoint to be used.

--- packages/utilities/shared/getServiceEndpoint.ts
+++ packages/utilities/shared/getServiceEndpoint.ts
@@ -2,9 +2,7 @@ const DEFAULT_SERVICE_ENDPOINT = "https://service.jamcomments.com";
 const DEVELOPMENT_SERVICE_ENDPOINT = "http://localhost:4000";
 
 const getServiceEndpoint = (): string => {
-  const processObject: { [key: string]: any } = typeof process !== "undefined" ? process : {};
-
-return processObject?.env?.NODE_ENV === "production" ? DEFAULT_SERVICE_ENDPOINT : DEVELOPMENT_SERVICE_ENDPOINT;
+return process.env.NODE_ENV === "production" ? DEFAULT_SERVICE_ENDPOINT : DEVELOPMENT_SERVICE_ENDPOINT;
 };
 
 export default getServiceEndpoint;
