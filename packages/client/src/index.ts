f62aa550ad12065af4d626be8996b46bc9ab38e1
Alex MacArthur <alex@macarthur.me>
2022-11-05 23:26:29 -0500

    Ensure that the comment controller is initialized only once.

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,8 +1,19 @@
 import CommentController from "./CommentController";
 
-export const initialize = (root) => {
-    return new CommentController({
-        root,
+const JamCommentsMap = new Map();
+
+export const initialize = (root: HTMLElement | Element): typeof CommentController => {
+    if(JamCommentsMap.has(root)) {
+        console.log('Already initialized for node:', root);
+        return JamCommentsMap.get(root);
+    }
+
+    const controllerInstance = new CommentController({
+        root: root as HTMLElement,
     });
+
+    JamCommentsMap.set(root, controllerInstance);
+
+    return controllerInstance;
 }
 
