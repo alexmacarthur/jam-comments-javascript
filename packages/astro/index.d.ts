76b2c592053073829f1d9fd57421080e0eb03b16
Alex MacArthur <alex@macarthur.me>
2023-03-24 23:16:21 -0500

    Bump Eleventy dependencies.

--- packages/astro/index.d.ts
+++ packages/astro/index.d.ts
@@ -1,1 +1,13 @@
-export {};
+declare global {
+  interface Window {
+    JamComments: {
+      initialize: () => void;
+    };
+  }
+}
+
+export interface JamCommentsProps {
+  path: string;
+}
+
+export default function JamComments({ path }: JamCommentsProps): JSX.Element;
