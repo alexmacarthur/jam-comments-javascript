4acf4dcd22b0107cff8d710e5eb57d7c9818fd44
Alex MacArthur <alex@macarthur.me>
2024-07-19 22:18:25 -0500

    fixing up astro

--- packages/astro/src/utils.test.ts
+++ packages/astro/src/utils.test.ts
@@ -1,23 +1,48 @@
-import { expect, it } from "vitest";
-import { getCurrentPath } from "./utils";
+import { describe, expect, it } from "vitest";
+import { getCurrentPath, removeFalseyValues } from "./utils";
 import { AstroGlobal } from "astro";
 
-it("should return the current path", () => {
-  const astro = {
-    request: {
-      url: "https://example.com/test",
-    },
-  } as AstroGlobal;
+describe("getCurrentPath()", () => {
+  it("should return the current path", () => {
+    const astro = {
+      request: {
+        url: "https://example.com/test",
+      },
+    } as AstroGlobal;
 
-  expect(getCurrentPath(astro)).toBe("/test");
+    expect(getCurrentPath(astro)).toBe("/test");
+  });
+
+  it("should ignore query parameters", () => {
+    const astro = {
+      request: {
+        url: "https://example.com/test?query=param",
+      },
+    } as AstroGlobal;
+
+    expect(getCurrentPath(astro)).toBe("/test");
+  });
 });
 
-it("should ignore query parameters", () => {
-  const astro = {
-    request: {
-      url: "https://example.com/test?query=param",
-    },
-  } as AstroGlobal;
+describe("removeFalseyValues()", () => {
+  it("should remove falsey values", () => {
+    expect(removeFalseyValues({ a: 1, b: 0, d: null, e: "" })).toEqual({
+      a: 1,
+    });
+  });
 
-  expect(getCurrentPath(astro)).toBe("/test");
+  it("should remove undefined items", () => {
+    const customCopy = {
+      copy_confirmation_message: "Thank you for your comment!",
+      copy_submit_button: "Submit",
+      copy_name_placeholder: undefined,
+    };
+
+    const result = removeFalseyValues(customCopy);
+
+    expect(result).toEqual({
+      copy_confirmation_message: "Thank you for your comment!",
+      copy_submit_button: "Submit",
+    });
+  });
 });
--- packages/astro/src/utils.ts
+++ packages/astro/src/utils.ts
@@ -3,3 +3,9 @@ import { AstroGlobal } from "astro";
 export function getCurrentPath(astro: AstroGlobal) {
   return new URL(astro.request.url).pathname;
 }
+
+export function removeFalseyValues<T>(obj: T): Partial<T> {
+  const filteredItems = Object.entries(obj).filter(([, value]) => value);
+
+  return Object.fromEntries(filteredItems) as Partial<T>;
+}
