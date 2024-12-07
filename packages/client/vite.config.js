897fcc6f1050c5837a21bf4e521f5b9767c9d05f
Alex MacArthur <alex@macarthur.me>
2024-12-06 22:41:52 -0600

    Introduce web component.

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -11,7 +11,6 @@ dotenv.config();
 export default defineConfig({
   test: {
     environment: "happy-dom",
-    setupFiles: ["./src/setupTests.ts"],
   },
   define: {
     "process.env.NODE_ENV": `"${process.env.NODE_ENV}"`,
@@ -19,9 +18,18 @@ export default defineConfig({
   build: {
     ...baseViteBuildOptions,
     lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
+      entry: [
+        path.resolve(__dirname, "src/index.ts"),
+        path.resolve(__dirname, "src/component.ts"),
+      ],
       name: "JamComments",
-      fileName: (format) => `index.${format}.js`,
+      fileName: (format, entryName) => {
+        if (entryName === "component") {
+          return `component/index.js`;
+        }
+
+        return `${entryName}.${format}.js`;
+      },
     },
     rollupOptions: {
       ...baseRollupOptions,
